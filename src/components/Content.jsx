b50dead50586892ad88174acc093259b5c4191c0
Jesse Alvarado <contact@jessealvarado.com>
2021-12-21 11:20:50 +0900

    add content to components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Content() {
   return (
     <div>
-      Content
+      <h2>About</h2>
+      <p>I'm a team-oriented fullstack developer with a background in business and information technology.I recently decided to move back and focus on software development, working on web projects using cutting edge technology such as Go(Golang), Google Cloud Platform (GCP), and modern frontend frameworks such as React and Svelte.
+</p>
+      <h2>Interests</h2>
+      <p>I love to be active and go out on the weekends.</p>
     </div>
   )
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Title() {
   return (
     <div>
-      Title
+      <img src="" alt=""/>
+      <h1>Jesse Alvarado</h1>
+      <h3>Frontend Developer</h3>
+      <p>Frontend Developer</p>
     </div>
   )
 }
