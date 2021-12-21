048dab894db96b0450ad0579606a30dc64691501
Jesse Alvarado <contact@jessealvarado.com>
2021-12-21 12:54:20 +0900

    add buttons to top nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div>
+    <div className='app'>
       <Title />
       <Content />
       <Footer />
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,3 +1,6 @@
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import profile from '../assets/profile.png'
 
@@ -8,6 +11,9 @@ export default function Title() {
       <h1>Jesse Alvarado</h1>
       <h3>Frontend Developer</h3>
       <p>Frontend Developer</p>
+
+      <a className='btn'><FontAwesomeIcon icon={faEnvelope} />Email</a>
+      <a className='btn btn-primary'><FontAwesomeIcon icon={faLinkedin} />LinkedIn</a>
     </nav>
   )
 }
