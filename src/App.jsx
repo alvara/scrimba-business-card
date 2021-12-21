fad5bc502147b52bd46b174400550be1960dfa45
Jesse Alvarado <contact@jessealvarado.com>
2021-12-21 10:16:49 +0900

    setup components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from './components/Title'
+import Content from './components/Content'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Title />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
